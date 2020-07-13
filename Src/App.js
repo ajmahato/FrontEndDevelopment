fdb6cd710b7e745af2559431ec40afc73e40415d
aj9775 <37789394+aj9775@users.noreply.github.com>
2020-07-13 11:08:44 +0530

    Create App.js

---
+++ Src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Counter from './count';
+import ToDo from './ToDo';
+import Todo from './ToDo';
+
+const App=()=> {
+  return (
+       <Counter />
+      //<Todo />
+  );
+}
+
+export default App;
